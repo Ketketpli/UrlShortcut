8c675205f267af06c374a709efb29c842c253f1d
Paulo Henrique <varelapaulo852@gmail.com>
2026-05-28 16:35:00 -0300

    Redirect user

--- src/main/java/com/urlKatz/LambdaService/RedirectLambda.java
+++ src/main/java/com/urlKatz/LambdaService/RedirectLambda.java
@@ -1,4 +1,39 @@
 package com.urlKatz.LambdaService;
 
-public class RedirectLambda {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RedirectLambda implements RequestHandler<Map<String, Object>, Map<String, String>> {
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+
+        String shortUrl = (String) input.get("shortUrl");
+
+        DynamoDbClient dynamoDB = DynamoDbClient.builder()
+                .region(Region.of("sa-east-1"))
+                .build();
+
+        Map<String, AttributeValue> key = new HashMap<>();
+
+        key.put("shortUrl", AttributeValue.builder().s(shortUrl).build());
+
+        GetItemRequest getItemRequest = GetItemRequest.builder()
+                .tableName("shortAWS")
+                .key(key)
+                .build();
+
+        dynamoDB.getItem(getItemRequest);
+
+        return Map.of(
+                "shortUrl", shortUrl
+        );
+    }
 }
